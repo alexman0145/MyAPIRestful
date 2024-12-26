949fe65d4a6f687c2dd562bb98f332c71f520cb7
Rigaud Alex <Bousix145@outlook.fr>
2024-12-26 18:25:20 +0100

    Fix setup script: correct container name typo, update migration project name, and streamline database migration process

--- setup.sh
+++ setup.sh
@@ -4,14 +4,13 @@ set -e # arrête le script en cas d'erreur
 
 # Variables
 CONTAINER_NAME="mssqlserver"
-SQL_FILE="./OwlCreate.sql"
 PASSWORD="Password123456789"
 SQLCMD_PATH="/opt/mssql-tools/bin/sqlcmd"
-MIGRATION_PROJECT="MigrationProject"
+MIGRATION_PROJECT="MonApiRestful"
 DOTNET_TEST_PROJECT="MonApiTests"
 
 start_container() {
-    echo "Starting the docker containter..."
+    echo "Starting the docker container..."
     docker-compose up -d
     echo "Container started."
 }
@@ -26,22 +25,10 @@ wait_for_sql_server() {
 }
 
 run_migration() {
-
-echo "Running database migration..."
-dotnet ef database update --project $MIGRATION_PROJECT --startup-project $MIGRATION_PROJECT 
-echo "Database migration completed."
+    echo "Running database migration..."
+    dotnet ef database update --project "$MIGRATION_PROJECT" --startup-project "$MIGRATION_PROJECT" 
+    echo "Database migration completed."
 }
-
-copy_and_execute_sql() {
-    echo "Copying the database setup script into the container..."
-    docker cp $SQL_FILE $CONTAINER_NAME:/tmp/OwlCreate.sql
-
-    echo "Running the SQL script..."
-    docker exec $CONTAINER_NAME $SQLCMD_PATH -S localhost -U SA -P "$PASSWORD" -d master -i /OwlCreate.sql
-    echo "Database setup complete."
-}
-
-
 
 run_tests() {
     echo "Running tests in $DOTNET_TEST_PROJECT..."
@@ -55,7 +42,7 @@ run_tests() {
 start_container
 wait_for_sql_server
 run_migration
-copy_and_execute_sql
 run_tests
 
 echo "All tasks completed successfully."
+
